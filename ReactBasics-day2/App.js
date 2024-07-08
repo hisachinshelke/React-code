6789bccf2a73284d2c611ba8742e37919ddb048a
hisachinshelke <sachinshelke282@gmail.com>
2024-07-08 19:41:55 +0530

    React basics day-2

---
+++ ReactBasics-day2/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+                    "h1", 
+                    {id:"heading",
+                     prp: "xyz"   
+                    }, 
+                    "Hello from React code!!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
